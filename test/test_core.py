c7b4989383b2b25933e8be0dff6e73ba6dfa8453
AlvaroMarques <sc.poi458@gmail.com>
2026-01-10 21:36:31 -0300

    reading json content

--- test/test_core.py
+++ test/test_core.py
@@ -1,6 +1,7 @@
 import pytest
 from api.core import *
 from importlib.resources import files
+from pathlib import Path
 
 
 def test_secret():
@@ -13,7 +14,12 @@ def test_client():
     client = make_client("OPENAI_API_KEY")
     model_name = load_model_name("API_MODEL_ID")
     print(model_name)
-    prompt = (files("api.prompts") / "00.md").read_text()
+    prompt = (files("api.prompts") / "01.md").read_text()
+    json_information_path = Path(__file__).parent / "data" / "output.json"
+    with open(json_information_path, "r") as json_f:
+        json_information = json_f.read()
+
+    prompt = "\n".join([prompt, json_information])
     print(prompt)
 
     print(get_response(client, model_name, prompt))
