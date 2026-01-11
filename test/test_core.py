c40f4ad23db910a6ccec74e0bfd311b66cfff550
AlvaroMarques <sc.poi458@gmail.com>
2026-01-10 21:22:45 -0300

    We can read prompts now

--- test/test_core.py
+++ test/test_core.py
@@ -1,5 +1,6 @@
 import pytest
 from api.core import *
+from importlib.resources import files
 
 
 def test_secret():
@@ -12,5 +13,7 @@ def test_client():
     client = make_client("OPENAI_API_KEY")
     model_name = load_model_name("API_MODEL_ID")
     print(model_name)
-    prompt = "Hello!"
+    prompt = (files("api.prompts") / "00.md").read_text()
+    print(prompt)
+
     print(get_response(client, model_name, prompt))
